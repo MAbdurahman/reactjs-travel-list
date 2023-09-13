bc933c15bcbd417c4d7e55adb647561c7564c960
MAbdurahman <mdbdrrhm@gmail.com>
2023-09-13 16:39:39 -0500

    Add onSubmit and onChange function to components/Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,33 +1,46 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
+import swal from 'sweetalert';
 
 export default function Form() {
-    const [description, setDescription] = useState("");
-    const [quantity, setQuantity] = useState(1);
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log('handleSubmit called', description);
-        setDescription('')
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) {
+        swal("Invalid Description!", "Must be a valid description", "error");
+        return;
     }
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+      console.log(newItem);
 
-    return (
-        <form className="add-form" onSubmit={handleSubmit}>
-            <h3>What do you need for your 😍 trip?</h3>
-            <select name="" id="">
-                {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-                    <option value={num} key={num}>
-                        {num}
-                    </option>
-                ))}
-            </select>
-            <input
-                type="text"
-                placeholder="Item..."
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-            />
-            <button>Add Item</button>
-        </form>
+    setDescription("");
+    setQuantity(1);
+  }
 
-    );
-};
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        name=""
+        id=""
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add Item</button>
+    </form>
+  );
+}
