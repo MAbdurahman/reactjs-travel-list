698bc64e3f4546ed1afa2cdeceb5627f8a9ac268
MAbdurahman <mdbdrrhm@gmail.com>
2023-09-13 09:30:18 -0500

    Modify: add basic structure for components/Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Form() {
 
+
     return (
-        <div className="add-form">
+        <form className="add-form">
             <h3>What do you need for your 😍 trip?</h3>
-        </div>
+            <select name="" id="">
+                {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+                    <option value={num} key={num}>
+                        {num}
+                    </option>
+                ))}
+            </select>
+            <input
+                type="text"
+                placeholder="Item..."
+                value={'socks'}
+            />
+            <button>Add Item</button>
+        </form>
 
     );
 };
