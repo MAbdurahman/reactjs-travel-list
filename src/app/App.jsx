2e0bea46b4f15db4df76fd746943d335078c99d1
MAbdurahman <mdbdrrhm@gmail.com>
2023-09-15 07:37:26 -0500

    Complete project - not responsive

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,6 +3,7 @@ import Header from "../layouts/Header";
 import Form from "../components/Form";
 import PackingList from "../components/PackingList";
 import Footer from "../layouts/Footer";
+import swal from 'sweetalert';
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -23,11 +24,30 @@ export default function App() {
         );
     }
 
+    function handleClearList () {
+        swal({
+            title: 'Clear List, are you sure?',
+            text: 'Once deleted, impossible to recover!',
+            icon: 'warning',
+            buttons: true,
+            dangerMode: true,
+        }).then(willDelete => {
+            if (willDelete) {
+                setItems([]);
+                swal('Your items have been deleted!', {
+                    icon: 'success',
+                });
+            } else {
+                swal('Your items are safe!');
+            }
+        });
+    }
+
   return (
     <div className="app">
       <Header />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} onClearList={handleClearList} />
       <Footer items={items} />
     </div>
   );
--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import PackingItem from "./PackingItem";
 
-export default function PackingList({ items, onDeleteItem, onToggleItem }) {
+export default function PackingList({
+  items,
+  onDeleteItem,
+  onToggleItem,
+  onClearList,
+}) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+
+  if (sortBy === "input") {
+    sortedItems = items;
+  }
+
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+
+  if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+  console.log(sortedItems)
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
-          <PackingItem key={item.id} item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem}/>
+        {sortedItems.map((item) => (
+          <PackingItem
+            key={item.id}
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onClearList}>Clear list</button>
+      </div>
     </div>
   );
 }
